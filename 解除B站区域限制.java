75d0f0025d36cea17b9a9382b4daf99bf49045f7
せいじゅ <124672990+yudonw@users.noreply.github.com>
2025-01-24 12:09:54 +0800

    Update 解除B站区域限制.java
    修复小错误，删除无用行。

--- 解除B站区域限制.java
+++ 解除B站区域限制.java
@@ -1,49 +1,43 @@
+// ==UserScript==
+// @name         解除B站区域限制_辅助工具
+// @namespace    http://tampermonkey.net/
+// @version      1.5
+// @description  修复使用解除B站区域限制的小问题
+// @author       三年二班
+// @match        *://www.bilibili.com/*
+// @grant        none
+// @license      MIT
+// @downloadURL https://update.greasyfork.org/scripts/524497/%E8%A7%A3%E9%99%A4B%E7%AB%99%E5%8C%BA%E5%9F%9F%E9%99%90%E5%88%B6_%E8%BE%85%E5%8A%A9%E5%B7%A5%E5%85%B7.user.js
+// @updateURL https://update.greasyfork.org/scripts/524497/%E8%A7%A3%E9%99%A4B%E7%AB%99%E5%8C%BA%E5%9F%9F%E9%99%90%E5%88%B6_%E8%BE%85%E5%8A%A9%E5%B7%A5%E5%85%B7.meta.js
+// ==/UserScript==
+
 (function() {
     'use strict';
-
-    // 获取当前页面的URL
     let url = window.location.href;
-
-    // 获取网页标题
     let title = document.title;
-
-    // 繁体字的正则表达式
     let regexTraditional = /[\u3400-\u4DBF\uF900-\uFAFF\u2E80-\u2EFF\u31C0-\u31EF\u2F00-\u2FDF\u2FF0-\u2FFF\u3000-\u303F\u31A0-\u31BF\u4DC0-\u4DFF\uA490-\uA4CF\uA000-\uA48F\uA4D0-\uA4FF\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\uFF00-\uFFEF]/;
 
-    // 检查是否是哔哩哔哩的网站
     if (url.includes("bilibili.com/bangumi/play")) {
-        // 检查标题中是否包含“僅限”或包含繁体字
+
         if (title.includes("僅限") || regexTraditional.test(title)) {
-            // 如果URL没有查询标识
             if (!url.includes("?") && !url.includes("spm_id_from")) {
-                // 在URL后添加查询参数
                 let newUrl = url + "?spm_id_from=333.1387.0.0";
-
-                // 跳转到新URL
                 window.location.href = newUrl;
-                return; // 跳转后结束插件运行
+                return;
             }
-
-            // 检查URL中是否包含“from”字符段
             if (url.includes("from")) {
-                // 检查URL中是否包含“？”符号
                 if (url.includes("?")) {
-                    // 去掉“？”符号
                     let newUrl = url.replace("?", "");
-
-                    // 重新加载页面
                     window.location.href = newUrl;
                 }
             }
         }
     }
-
-    // 在页面加载完成后重新执行本插件
     window.addEventListener('load', function() {
         let url = window.location.href;
         let title = document.title;
 
-        if (url.includes("bilibili.com")) {
+        if (url.includes("bilibili.com/bangumi/play")) {
             if (title.includes("僅限") || regexTraditional.test(title)) {
                 if (url.includes("from_spmid")) {
                     if (url.includes("?")) {
@@ -56,5 +50,4 @@
     });
 
 })();
-
 
